6e14c6bfabf29273a5f563d3e356c937f4d073dc
PrashantChitte <prashant201291@gmail.com>
2017-01-29 12:30:30 +0530

    4th commit  in new repository

--- src/main/java/crudoperation/OperationOnProduct.java
+++ src/main/java/crudoperation/OperationOnProduct.java
@@ -14,21 +14,15 @@ import com.utility.UtilityClass;
  * @author PrashantChitte
  *
  */
-public class OperationOnProduct extends AbstractProductOperation 
-{
-	
-	
+public class OperationOnProduct extends AbstractProductOperation {
 
 	@Override
 	public boolean addProduct(ProductDetails objectproduct) {
-		Session ses=(Session) UtilityClass.getSessionFactory().openSession();
-		if(null==objectproduct)
-		{
+		Session ses = (Session) UtilityClass.getSessionFactory().openSession();
+		if (null == objectproduct) {
 			return false;
-		}
-		else
-		{
-			Transaction tx=ses.beginTransaction();
+		} else {
+			Transaction tx = ses.beginTransaction();
 			ses.save(objectproduct);
 			tx.commit();
 			ses.close();
@@ -37,73 +31,98 @@ public class OperationOnProduct extends AbstractProductOperation
 	}
 
 	@Override
-	public boolean removeProduct(String productName) {
+	public boolean removeProduct(String newName) {
+
+		Session ses = (Session) UtilityClass.getSessionFactory().openSession();
+		Transaction tx=ses.beginTransaction();
 		
-		Session ses=(Session) UtilityClass.getSessionFactory().openSession();
-		if(null!=productName)
-		ses.delete(productName,new ProductDetails());
-		ses.close();		
+		String hqlUpdate = "delete ProductDetails p where p.product_Name = :oldProdName";
+		int updatedEntities = ses.createQuery(hqlUpdate).setString("oldProdName", newName)
+		.executeUpdate();
+
+		tx.commit();
+			ses.close();
+			System.out.println(updatedEntities);
 		return true;
 	}
+
 	@Override
-	public boolean removeProduct(int prodId) {
+	public boolean removeProduct(int newprodId) {
+
+		Session ses = (Session) UtilityClass.getSessionFactory().openSession();
+		Transaction tx=ses.beginTransaction();
 		
-		Session ses=(Session) UtilityClass.getSessionFactory().openSession();		
-		ProductDetails ProdDetail=new ProductDetails();
-		ProdDetail.setProduct_Id(prodId);
-		ses.delete(ProdDetail);
-		ses.close();		
+		String hqlUpdate = "delete ProductDetails p where p.product_Id = :prodId";
+		int updatedEntities = ses.createQuery(hqlUpdate).setInteger("prodId", newprodId)
+		.executeUpdate();
+
+		tx.commit();
+			ses.close();
 		return true;
 	}
+
 	@Override
-	public boolean updateProduct(String prodName,double newProdPrice) {
-		Session ses=(Session) UtilityClass.getSessionFactory().openSession();		
-		ProductDetails ProdDetail=new ProductDetails();
-		ProdDetail.setProduct_Price(newProdPrice);;
-		ses.update(ProdDetail);
-		ses.close();		
-		return true;
+	public boolean updateProduct(String prodName, double newProdPrice) {
+		Session ses = (Session) UtilityClass.getSessionFactory().openSession();
+		Transaction tx = ses.beginTransaction();
 		
+		String hqlUpdate = "update ProductDetails p set p.product_Price = :newProdPrice where p.product_Name = :prodName";
+				int updatedEntities = ses.createQuery(hqlUpdate).setDouble("newProdPrice", newProdPrice)
+				.setString("prodName", prodName).executeUpdate();
+		ses.flush();
+		tx.commit();
+		ses.close();
+		if (updatedEntities > 0) {
+			System.out.println("Record Updated");
+			return true;
+		} else
+			return false;
+
 	}
+
 	@Override
-	public  boolean updateProduct(String prodName,String newProdName){
-		System.out.println(prodName+newProdName);
-		Session ses=(Session) UtilityClass.getSessionFactory().openSession();													
-					String hql = "UPDATE ProductDetails set NAME = :newProdName "  + 
-				                     "WHERE  NAME = :prodName";
-				Query query = ses.createQuery(hql);				
-				query.setParameter("newProdName", newProdName);
-				query.setParameter("prodName",prodName );
-				int result = query.executeUpdate();
-				System.out.println("result");
-				if(result>0)
-					return true;
-				else
-					return false;
+	public boolean updateProduct(String prodName, String newProdName) {
+		System.out.println(prodName + newProdName);
+		Session ses = (Session) UtilityClass.getSessionFactory().openSession();
+		Transaction tx = ses.beginTransaction();
+		
+		String hqlUpdate = "update ProductDetails p set p.product_Name = :newProdName where p.product_Name = :prodName";
+				int updatedEntities = ses.createQuery(hqlUpdate).setString("newProdName", newProdName)
+				.setString("prodName", prodName).executeUpdate();
+		ses.flush();
+		tx.commit();
+		ses.close();
+		if (updatedEntities > 0) {
+			System.out.println("Inserted");
+			return true;
+		} else
+			return false;
 	}
-	
+
 	@Override
 	public boolean retriveProduct(int prodId) {
-		Session ses=(Session) UtilityClass.getSessionFactory().openSession();		
-		Transaction tx=ses.beginTransaction();
-		ProductDetails proddetail=(ProductDetails) ses.get(ProductDetails.class, prodId);//get(ProductDetails.class,prodName);
-		
-		System.out.println("Product ID:"+proddetail.getProduct_Id());
-		System.out.println("Product Name:"+proddetail.getProduct_Name());
-		System.out.println("Product Price:"+proddetail.getProduct_Price());
-		System.out.println("Product Type:"+proddetail.getProductType());
-		System.out.println("Product Quantity:"+proddetail.getProductQuantity());
+		Session ses = (Session) UtilityClass.getSessionFactory().openSession();
+		Transaction tx = ses.beginTransaction();
+		ProductDetails proddetail = (ProductDetails) ses.get(ProductDetails.class, prodId);// get(ProductDetails.class,prodName);
+
+		System.out.println("Product ID:" + proddetail.getProduct_Id());
+		System.out.println("Product Name:" + proddetail.getProduct_Name());
+		System.out.println("Product Price:" + proddetail.getProduct_Price());
+		System.out.println("Product Type:" + proddetail.getProductType());
+		System.out.println("Product Quantity:" + proddetail.getProductQuantity());
 		return true;
 	}
 
 	public static void main(String[] args) {
-		OperationOnProduct op=new OperationOnProduct();
-		//ProductDetails proddetails=new ProductDetails("MotoG2", 14000, "Moblie", 1); 
-		//System.out.println(op.addProduct(proddetails));
-		//System.out.println(op.retriveProduct(1));
-		System.out.println(op.updateProduct("MotoG2", "Mpple"));
+		OperationOnProduct op = new OperationOnProduct();
+		// ProductDetails proddetails=new ProductDetails("MotoG2", 14000,
+		// "Moblie", 1);
+		// System.out.println(op.addProduct(proddetails));
+		// System.out.println(op.retriveProduct(1));
+		//System.out.println(op.updateProduct("MotoG2", "Mpple"));
+		//System.out.println(op.updateProduct("Mpple", 500));
+		//op.removeProduct(5);
+		op.removeProduct("Mpple");
 	}
-		
-	
 
 }
